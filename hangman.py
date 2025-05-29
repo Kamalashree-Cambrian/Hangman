ec651b80751efc6abe653ebbcc04c5ecaade36b5
Kamalashree-Cambrian <153592606+Kamalashree-Cambrian@users.noreply.github.com>
2025-05-29 10:31:36 +0530

    Created hangman.py

---
+++ hangman.py
@@ -0,0 +1,20 @@
+import random
+
+with open("words.txt") as f:
+    word = f.read().splitlines()
+
+word = random.choice(word)
+tries = 6
+guessed = set()
+
+while tries>0:
+    display = "".join([c if c in guessed else "_ " for c in word])
+    print("word: ",display)
+
+    if "_" not in display:
+        print("you won !")
+    guess = input("enter a letter: ")
+    if guess in word:
+        guessed.add(guess)
+    else:
+        tries -=1
